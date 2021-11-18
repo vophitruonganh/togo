a57b1f9ad672b96f08a656ed0b66c68c05880970
truonganhvo <truonganh.vo@popsww.com>
2021-11-18 12:54:00 +0700

    Check userinfo in token

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,11 +1,11 @@
 const authUtils = require('../ultils/auth.js');
-const {decrypt} = require('../commons/crypto.js');
 
 module.exports = (req, res, next) => {
 	const token = req.headers.authorization;
 
 	try {
 		req.userInfo = authUtils.verifyJWTToken(token);
+		if (!req.userInfo) throw new Error(`Token is invalid`);
 	} catch (error) {
 		console.error(`Validate token: `, error.message);
 		return res.status(401).send({message: 'PERMISSION DENIED'});
