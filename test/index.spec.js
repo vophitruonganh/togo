f01e89d8aaed32241429485e9370faacb43b6240
truonganhvo <truonganh.vo@popsww.com>
2021-11-20 08:45:35 +0700

    Update e2e test

--- test/index.spec.js
+++ test/index.spec.js
@@ -31,12 +31,27 @@ describe('POST /register', function() {
 });
 
 describe('GET /tasks', function() {
-	it('get task error', function(done) {
+	it('get task - not embed token header - error', function(done) {
 		request(app)
 			.get('/tasks?created_date=2020-06-29&page=1&limit=100')
 			.set('Authorization', 'abc')
 			.set('Accept', 'application/json')
 			.expect(401, done);
+	});
+
+	it('get task - embeded token header - emtpy response', async () =>{
+		const data = await request(app)
+			.get('/login?user_id=test_user_id_1&password=123456')
+			.set('Accept', 'application/json');
+
+		const response = await request(app)
+			.get('/tasks?created_date=2030-06-29&page=1&limit=100')
+			.set('Authorization', data.body.data.token)
+			.set('Accept', 'application/json')
+			.expect(200);
+
+		expect(response).not.toBeUndefined();
+		expect(response.body.data.length).toEqual(0);
 	});
 
 	it('get task success - has data', async function() {
@@ -45,13 +60,13 @@ describe('GET /tasks', function() {
 			.set('Accept', 'application/json');
 
 		const response = await  request(app)
-			.get('/tasks?created_date=2020-06-29')
+			.get('/tasks?created_date=2020-06-29&page=1&limit=100')
 			.set('Authorization', data.body.data.token)
 			.set('Accept', 'application/json')
 			.expect(200);
-		expect(response.body).not.toBeUndefined();
-		expect(response.body.data.length).toBeGreaterThan(0);
 
-		// done()
+		console.log(`response.body.data.length:`, response.body.data.length);
+		expect(response.body).not.toBeUndefined();
+		expect(response.body.data.length).toBeGreaterThanOrEqual(0);
 	});
 });
